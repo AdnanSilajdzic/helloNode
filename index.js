e48d1df6e7fa78ed2c59c5481efbd4489d818623
Adnan Silajdzic <adnan@omnitask-brera.localdomain>
2024-07-22 13:04:50 +0200

    Created a dev branch

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
     // Set the response HTTP header with HTTP status and Content type
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     // Send the response body "Hello World"
-    res.end('Hello World\n');
+    res.end('Hello Dev Branch!\n');
 });
 
 // Listen on port 3000, IP defaults to 127.0.0.1
